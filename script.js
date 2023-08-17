f0a6a8cd2abe296a3fa20c4553be7bdf8652a0e1
Simran Johal <134616481+simran-johal@users.noreply.github.com>
2023-08-17 18:31:20 +0100

    Add completed interactivity with grid functionality.
    - UI changes to buttons and page
    - Complete initial interaction functionality with grid

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 
 
 // creating the grid of pixels
-let containerDiv = document.getElementById('grid-container') // selected container div in html
+let gridContainerDiv = document.getElementById('grid-container') // selected container div in html
 let slider = document.getElementById('slider-scale') // selected slider
 let sliderNumber = document.getElementById('slider-value') // selected slider-value
 
-slider.addEventListener('input', function() { 
-    updateSliderValue()
-    createDivGrid()
-
-})
 
 function updateSliderValue() {
     sliderNumber.textContent = slider.value;
@@ -17,16 +12,59 @@ function updateSliderValue() {
 
 function createDivGrid() {
 
-    containerDiv.innerHTML = ''; // deletes inner html before the for loop
+    gridContainerDiv.innerHTML = ''; // deletes inner html before the for loop
 
-    containerDiv.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`
+    gridContainerDiv.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`
 
     for (let i = 0; i < slider.value * slider.value; i++) {
-        containerDiv.appendChild(document.createElement('div')) // creates divs inside
+        gridContainerDiv.appendChild(document.createElement('div')) // creates divs inside
     }
 }
 
+slider.addEventListener('input', () => { 
+    updateSliderValue()
+    createDivGrid()
+
+})
+
 updateSliderValue()
 createDivGrid()
+
+
+
+
+
+// use mouse events for the tracking using flags
+
+let isClicked = false
+
+gridContainerDiv.addEventListener('mousedown', (event) => {
+    if (event.target.tagName === "DIV") {
+        event.target.style.backgroundColor = "black"
+        isClicked = true
+    }
+})
+
+gridContainerDiv.addEventListener('mousemove', (event) => {
+    if (event.target.tagName === "DIV" && isClicked == true) {
+        event.target.style.backgroundColor = "black"
+    }
+})
+
+gridContainerDiv.addEventListener('mouseup', (event) => {
+    if (event.target.tagName === "DIV") {
+        event.target.style.backgroundColor = "black"
+        isClicked = false
+    }
+})
+
+// button for different settings
+
+//clear
+
+
+
+
+
 
 
